1f016d2ef4a1dab2e93902bb5c79bbb10c6977d1
lincong666 <995491026@qq.com>
2020-02-15 15:18:51 +0800

    second commit

--- vue-exampls/js/player.js
+++ vue-exampls/js/player.js
@@ -11,15 +11,45 @@
   2：歌曲url获取接口
   *请求地址：https://autumnfish.cn/song/url
   请求方法：get
-  请求参数：id
+  请求参数：id（歌曲id）
   响应内容：歌曲的url地址
+
+  3：歌曲详情信息（包括歌曲封面获取）
+  * 请求地址：https://autumnfish.cn/song/detail
+  *请求方法: get
+  *请求参数：ids（歌曲id）
+  *响应内容：歌曲详情包括封面
+  
+  4:歌曲评论的获取
+  * 请求地址：https://autumnfish.cn/comment/hot?type=0
+    请求方法：get
+    请求参数：id(歌曲id，地址中的type固定为0)
+    响应内容：歌曲的热门评论
+  * *
+* 5:mv地址的获取
+  * 请求地址：https://autumnfish.cn/mv/url
+    请求方法：get
+    请求参数：id(mvid，mvid=0时表示没有mv)
+    响应内容：mv的地址
+
  */
 var app = new Vue({
     el:"#player",
     data:{
         query:"",
         musicList:[],
-        musicUrl:""
+        //歌曲地址
+        musicUrl:"",
+        //歌曲封面
+        musicCover:"",
+        //歌曲评论
+        hotComments:[],
+        //动画播放状态
+        isPlaying:false,
+        //遮罩层的显示状态
+        isShow:false,
+        //mv的地址
+        mvUrl:""
     },
     methods: {
         searchMusic:function(){
@@ -33,12 +63,49 @@ var app = new Vue({
         playMusic:function(musicId){
             console.log(musicId);
             var that = this;
+            //获取歌曲地址（根据歌曲id来获取）
             axios.get("https://autumnfish.cn/song/url?id="+musicId)
             .then(function(response){
                 // console.log(response);
                 console.log(response.data.data[0].url);
                 that.musicUrl = response.data.data[0].url;
-            },function(){})
+            },function(err){});
+            //获取歌曲详情
+            axios.get("https://autumnfish.cn/song/detail?ids="+musicId)
+            .then(function(response){
+                console.log(response.data.songs[0].al.picUrl);
+                that.musicCover=response.data.songs[0].al.picUrl;
+            },function(err){});
+            //获取歌曲相应的评论
+            axios.get("https://autumnfish.cn/comment/hot?type=0&id="+musicId)
+            .then(function(response){
+                // console.log(response);
+                console.log(response.data.hotComments);
+                that.hotComments=response.data.hotComments;
+            },function(err){})
+        },
+        play:function(){
+            // console.log("play");
+            this.isPlaying=true;
+        },
+        pause:function(){
+            // console.log("pause");
+            this.isPlaying=false;
+        },
+        //播放mv
+        playMv:function(mvid){
+            var that = this;
+            axios.get("https://autumnfish.cn/mv/url?id="+mvid)
+            .then(function(response){
+                console.log(response.data.data.url);
+                that.isShow = true;
+                that.mvUrl = response.data.data.url;                
+            },function(err){})
+        },
+        //隐藏mv
+        hide:function(){
+            this.isShow = false;
+            this.mvUrl="";
         }
     },
 
